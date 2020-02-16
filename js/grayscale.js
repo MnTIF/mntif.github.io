ae06e6373b366974f1262878689eca44f14818f4
dxfg <37959945+dxfg@users.noreply.github.com>
2020-02-15 19:51:51 -0500

    Update grayscale.js

--- js/grayscale.js
+++ js/grayscale.js
@@ -6,10 +6,11 @@
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
+    $(".navbar-fixed-top").addClass("top-nav-collapse");
     //if ($(".navbar").offset().top > 50) {
     //    $(".navbar-fixed-top").addClass("top-nav-collapse");
     //} else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    //    $(".navbar-fixed-top").removeClass("top-nav-collapse");
     //}
 });
 
